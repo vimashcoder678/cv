b298e1c64e1a7575d1c28167924b97fd508bbb45
vimashcoder678 <114289402+vimashcoder678@users.noreply.github.com>
2022-10-13 19:55:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Vimash Prajapati</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Merriweather&family=Montserrat&family=Sacramento&display=swap" rel="stylesheet">
+	<link rel="icon" href="vp.ico">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	
+</head>
+<body>
+	<div class="top-container">
+		<img class="top_image " src="img/cloud.png" alt="cloud-img">
+		<h1>I' Vimash Prajapati.</h1>
+		<h2> a<span> pro </span>full stack web developer.</h2>
+		<img class="bottom_image" src="img/cloud.png" alt="cloud-img">
+		<img style="width: 60%; height: 80%" src="img/mountain.png" alt="mountain-img"> 
+	</div>
+	<div class="middle-container">
+		<div class="profile">
+            <img height="60px" src="img/boy.png" alt="Porfile_pic">
+            <h2>Hello.</h2>
+            <p>Lorem ipsum dolor sit amet, vitae volutpat, dui conubia, dolor urna. Non auctor, montes nulla distinctio.</p>
+       </div>
+    <hr>
+    <div class="skills">
+    	<h2>My Skills.</h2>
+        <div class="skill-row">
+            <img class="" src="img/coder.png" alt="">
+            
+            <h3>Full Stack Web Developer</h3>
+            <p>Lorem ipsum dolor sit amet, quis in duis, iaculis id felis. Consectetuer vestibulum, nunc urna lectus, erat ligula. Hendrerit nam, lectus ante, ut lorem eros.</p>
+       </div>
+       <hr>
+       <div class="skill-row">
+            <img class="" src="img/coders.png" alt="">
+            <h3>Java Developer</h3>
+            <p>Lorem ipsum dolor sit amet, mauris sed consectetuer. Etiam et eu, bibendum interdum, lacus quis mauris. Curabitur wisi, quisque vel eu, rutrum nam.</p>
+       </div>
+    </div>
+    <hr>
+        <div class="contact-me">
+            <h2>Get In Touch</h2>
+            <h3>Lorem ipsum dolor sit amet, non elit.</h3>
+            <p>Lorem ipsum dolor sit amet, in quis, aenean amet. Phasellus sodales, tellus donec dui, ornare erat.</p>
+            <a class="btn" href="mailto:name@email.com">CONTACT ME</a>
+        </div>
+	</div>
+	<div class="bottom-container">
+		<a class="footer-link" href="https://www.linkedin.com/">LinkedIn</a>
+        <a class="footer-link" href="https://twitter.com/">Twitter</a>
+        <a class="footer-link" href="https://www.appbrewery.co/">Website</a>
+        <p>© Vimash Prajapati.</p>
+	</div>
+</body>
+</html>
